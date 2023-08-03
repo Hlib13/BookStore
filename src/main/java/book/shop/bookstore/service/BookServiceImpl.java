26a157ab987ef18bea9da312241ff1fd11f1f62f
Gleb Chebotarov <taronassvolt@gmail.com>
2023-08-03 19:46:07 +0300

    book added

---
+++ src/main/java/book/shop/bookstore/service/BookServiceImpl.java
@@ -0,0 +1,25 @@
+package book.shop.bookstore.service;
+
+import book.shop.bookstore.model.Book;
+import book.shop.bookstore.repository.BookRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}

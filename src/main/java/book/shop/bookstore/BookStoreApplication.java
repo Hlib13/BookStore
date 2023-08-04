c39b50d55f17d6e4f2cc4b84f385a9d8406c494a
Gleb Chebotarov <taronassvolt@gmail.com>
2023-08-04 16:05:37 +0300

    fix

--- src/main/java/book/shop/bookstore/BookStoreApplication.java
+++ src/main/java/book/shop/bookstore/BookStoreApplication.java
@@ -1,9 +1,5 @@
 package book.shop.bookstore;
 
-import book.shop.bookstore.model.Book;
-import book.shop.bookstore.service.BookService;
-import java.math.BigDecimal;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,8 +7,6 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
-    @Autowired
-    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
@@ -21,14 +15,7 @@ public class BookStoreApplication {
     @Bean
     public CommandLineRunner commandLineRunner() {
         return args -> {
-            Book titenik = new Book();
-            titenik.setTitle("Titenik");
-            titenik.setAuthor("Someone");
-            titenik.setIsbn("134");
-            titenik.setPrice(BigDecimal.valueOf(999));
 
-            bookService.save(titenik);
-            System.out.println(bookService.findAll());
         };
     }
 

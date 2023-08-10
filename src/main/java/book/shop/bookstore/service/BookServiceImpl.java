de4e0a65b56a366068a454cca5a758d9ea4a5bf0
Gleb Chebotarov <taronassvolt@gmail.com>
2023-08-11 00:40:38 +0300

    swagger and pagination added

--- src/main/java/book/shop/bookstore/service/BookServiceImpl.java
+++ src/main/java/book/shop/bookstore/service/BookServiceImpl.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import book.shop.bookstore.repository.book.BookSpecificationBuilder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -33,8 +34,8 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> findAll() {
-        return bookRepository.findAll()
+    public List<BookDto> findAll(Pageable pageable) {
+        return bookRepository.findAll(pageable)
                 .stream()
                 .map(bookMapper::toDto)
                 .toList();

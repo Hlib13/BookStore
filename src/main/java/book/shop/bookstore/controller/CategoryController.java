a8b72ea04d66e713addf779ad2e0cda2de3fd40c
Gleb Chebotarov <taronassvolt@gmail.com>
2023-08-16 00:39:10 +0300

    category fix

--- src/main/java/book/shop/bookstore/controller/CategoryController.java
+++ src/main/java/book/shop/bookstore/controller/CategoryController.java
@@ -15,7 +15,6 @@ import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;

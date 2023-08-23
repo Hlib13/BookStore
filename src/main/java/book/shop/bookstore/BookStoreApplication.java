d6a8c1dd84500177f666ada9e3b2fe370f2c9673
Gleb Chebotarov <taronassvolt@gmail.com>
2023-08-23 17:39:18 +0300

    little fix

--- src/main/java/book/shop/bookstore/BookStoreApplication.java
+++ src/main/java/book/shop/bookstore/BookStoreApplication.java
@@ -3,6 +3,8 @@ package book.shop.bookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class BookStoreApplication {
 
